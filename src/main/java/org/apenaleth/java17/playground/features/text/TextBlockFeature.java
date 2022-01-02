e4aafefd71abf460b1d68fc1895f7ab0149a82c2
ampena <antonio.pena@externos.w2m.com>
2022-01-02 13:17:06 +0100

    Add TextBlockFeature

---
+++ src/main/java/org/apenaleth/java17/playground/features/text/TextBlockFeature.java
@@ -0,0 +1,14 @@
+package org.apenaleth.java17.playground.features.text;
+
+public class TextBlockFeature {
+
+    public static void main(String[] args) {
+        System.out.println("""
+                {
+                    "hero": "Thrall",
+                    "race": "Orc",
+                    "city": "Orgrimar"
+                }
+                """);
+    }
+}
